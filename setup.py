eb241a2271c21afb95aabb11c3d61ddf2340d499
Manes Hi <manes.hipocrates@gmail.com>
2021-03-04 20:07:21 +0100

    Refined directory using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Migrating from TF1 to TF2',
+    author='enate',
+    license='',
+)
